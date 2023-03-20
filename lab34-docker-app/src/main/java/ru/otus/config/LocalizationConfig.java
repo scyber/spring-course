5625be20a0dea6c305b1f503e9cdf4c2a1e67648
Grigory Piskunov <unixfan@yandex.ru>
2023-03-20 15:33:12 +0100

    init docker save.

--- lab34-docker-app/src/main/java/ru/otus/config/LocalizationConfig.java
+++ lab34-docker-app/src/main/java/ru/otus/config/LocalizationConfig.java
@@ -1,5 +1,6 @@
 package ru.otus.config;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
