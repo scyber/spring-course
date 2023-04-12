563847d5110fc99a9400f6ebbabfcb7bb7525f63
Grigory Piskunov <unixfan@yandex.ru>
2023-04-12 09:44:00 +0200

    Move requests to "External Requests".

--- lab38-spring-cloud/src/main/java/ru/otus/controllers/ClientLibraryRestController.java
+++ lab38-spring-cloud/src/main/java/ru/otus/controllers/ClientLibraryRestController.java
@@ -1,10 +1,8 @@
 package ru.otus.controllers;
 
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.domain.Author;
@@ -12,8 +10,6 @@ import ru.otus.domain.Book;
 import ru.otus.domain.Comment;
 import ru.otus.domain.Genre;
 import ru.otus.services.LibraryClientService;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,59 +19,45 @@ public class ClientLibraryRestController {
 
     private final LibraryClientService libraryClientService;
 
-    @HystrixCommand(fallbackMethod = "failGetAllBooks")
     @RequestMapping(method = RequestMethod.GET, value = "/client/api/books")
     Page<Book> getBooks(@RequestParam("page") Integer page, @RequestParam("size") Integer size){
         return libraryClientService.getPagebleBooks(page,size);
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.GET, value = "/client/api/book")
     Optional<Book> getBookById(Long id){
         return libraryClientService.getBookById(id);
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.POST, value = "/client/api/books", consumes = {MediaType.APPLICATION_JSON_VALUE})
     Book saveAndUpdateBook(@RequestBody Book book){
         return libraryClientService.saveAndUpdateBook(book);
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.DELETE, value = "/client/api/books")
     void deleteBookById(Long id){
         libraryClientService.deleteBookById(id);
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.POST, value = "/client/api/books")
     void updateBookTitleById( Long id, String title){
         libraryClientService.updateBookTitleById(id,title);
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.GET, value = "/client/api/authors")
     public List<Author> getAuthors() {
         return libraryClientService.getAllAuthors();
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.GET, value = "/client/api/genres")
     public List<Genre> getGenres(){
         return libraryClientService.getAllGenres();
     }
 
-    @HystrixCommand
     @RequestMapping(method = RequestMethod.GET, value = "/client/api/comments")
     public List<Comment> getComments(){
         return libraryClientService.getAllComments();
     }
 
-
-    private Page<Book> failGetAllBooks(Integer page, Integer size){
-        var emptyList = new ArrayList<Book>();
-        var emptyBookPage = new PageImpl<>(emptyList);
-        return emptyBookPage;
-    }
 
 }
--- lab38-spring-cloud/src/main/java/ru/otus/services/LibraryClientServiceImpl.java
+++ lab38-spring-cloud/src/main/java/ru/otus/services/LibraryClientServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.otus.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.otus.clients.LibraryClient;
@@ -10,6 +12,8 @@ import ru.otus.domain.Book;
 import ru.otus.domain.Comment;
 import ru.otus.domain.Genre;
 import ru.otus.exeptions.FindItemExecption;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,36 +23,43 @@ public class LibraryClientServiceImpl implements LibraryClientService {
 
     private final LibraryClient libraryClient;
 
+    @HystrixCommand(fallbackMethod = "failGetAllBooks")
     @Override
     public Page<Book> getPagebleBooks(Integer page, Integer size) {
        return libraryClient.getBooks(PageRequest.of(page,size));
     }
 
+    @HystrixCommand
     @Override
     public Optional<Book> getBookById(Long id) {
         return Optional.of(libraryClient.getBookById(id).orElseThrow(() -> new FindItemExecption("Could not find Book with id " + id)));
     }
 
+    @HystrixCommand
     @Override
     public Book saveAndUpdateBook(Book book) {
         return libraryClient.saveAndUpdateBook(book);
     }
 
+    @HystrixCommand
     @Override
     public void deleteBookById(Long id) {
         libraryClient.deleteBookById(id);
     }
 
+    @HystrixCommand
     @Override
     public void updateBookTitleById(Long id, String title) {
         libraryClient.updateBookTitleById(id,title);
     }
 
+    @HystrixCommand
     @Override
     public List<Author> getAllAuthors() {
         return libraryClient.getAllAuthors();
     }
 
+    @HystrixCommand
     @Override
     public List<Genre> getAllGenres() {
         return libraryClient.getAllGenres();
@@ -59,4 +70,9 @@ public class LibraryClientServiceImpl implements LibraryClientService {
         return libraryClient.getAllComments();
     }
 
+    private Page<Book> failGetAllBooks(Integer page, Integer size){
+        var emptyList = new ArrayList<Book>();
+        var emptyBookPage = new PageImpl<>(emptyList);
+        return emptyBookPage;
+    }
 }
