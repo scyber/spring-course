519487569def369612334af89d4048a03cc63c29
Grigory Piskunov <unixfan@yandex.ru>
2023-05-07 19:14:17 +0200

    remove unused functions

--- spring-project-final/client/src/main/java/ru/otus/config/LibraryLoadBalancingConfiguration.java
+++ spring-project-final/client/src/main/java/ru/otus/config/LibraryLoadBalancingConfiguration.java
@@ -1,6 +1,5 @@
 package ru.otus.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.openfeign.support.PageJacksonModule;
 import org.springframework.cloud.openfeign.support.SortJacksonModule;
@@ -9,9 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.Module;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class LibraryLoadBalancingConfiguration {
@@ -40,17 +36,5 @@ public class LibraryLoadBalancingConfiguration {
         return new SortJacksonModule();
     }
 
-//    @Bean
-//    public FilterRegistrationBean corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
-//        filterRegistrationBean.setOrder(0);
-//        return filterRegistrationBean;
-//    }
+
 }
