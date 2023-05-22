96c85b05fc144f494ffdde47fab2f93da41e45bb
Grigory Piskunov <grigoriy.piskunov@mercedes-benz.com>
2023-05-22 10:50:12 +0200

    remove mongo useless type id.

--- spring-project-final/backend/src/main/java/ru/otus/domain/Author.java
+++ spring-project-final/backend/src/main/java/ru/otus/domain/Author.java
@@ -3,12 +3,8 @@ package ru.otus.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.FieldType;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 
 @Document(collection = "authors")
@@ -18,7 +14,6 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 public class Author {
 
     @Id
-    @MongoId(targetType = FieldType.OBJECT_ID)
     private String id;
 
     private String name;
